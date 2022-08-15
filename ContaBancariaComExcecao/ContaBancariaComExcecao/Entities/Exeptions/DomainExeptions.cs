ad30e4007419c4e12f28090600037a4b571c5833
Alex Messias <alexmessias_18@yahoo.com.br>
2022-08-14 21:26:57 -0300

    Exceções 2

---
+++ ContaBancariaComExcecao/ContaBancariaComExcecao/Entities/Exeptions/DomainExeptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContaBancariaComExcecao.Entities;
+
+namespace ContaBancariaComExcecao.Entities.Exeptions
+{
+    internal class DomainExeptions : ApplicationException
+    {
+        public DomainExeptions(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ ContaBancariaComExcecao/ContaBancariaComExcecao/Program.cs
@@ -0,0 +1,38 @@
+﻿using ContaBancariaComExcecao.Entities;
+using ContaBancariaComExcecao.Entities.Exeptions;
+using System.Globalization;
+
+namespace ContaBancariaComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int name = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("WithDraw Limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(name, holder, initialBalance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double withDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.Withdraw(withDraw);
+                Console.WriteLine(acc);
+            }
+            catch (DomainExeptions e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
